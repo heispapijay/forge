e5b87a29ee1e9e509dd9a19a549036b5a2a0539e
heispapijay <heispapijay@gmail.com>
2024-02-28 16:29:53 +0200

    Remove unused imports and commented code in Carousel and Navbar components

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Marquee from "react-fast-marquee";
 import top from "../../assets/top.png";
 import bottom from "../../assets/bottom.png";
@@ -6,28 +5,11 @@ import "./carousel.scss";
 
 export const Carousel = () => {
   return (
-    // <div className="carousel">
-    //   <div className="whitecover-top"></div>
-    //   <div className="carousel-container">
-    //     <Marquee>
-    //       <div className="image1"></div>
-    //       <div className="image1"></div>
-    //       <div className="image1"></div>
-    //       <div className="image1"></div>
-    //       <div className="image1"></div>
-    //     </Marquee>
-    //   </div>
-    //   <div className="whitecover-bottom"></div>
-    // </div>
 
     <div className="caro">
         <div className="overlay">
             <img className="whitecover-top" src={top} alt="whitespace" />
-            <img className="whitecover-bottom" src={bottom} alt="whitespace" />
-
-        {/* <div className="whitecover-top"></div>
-        <div className="whitecover-bottom"></div> */}
-        </div>
+            <img className="whitecover-bottom" src={bottom} alt="whitespace" />        </div>
         <div className="carousel-container">
          <Marquee>
            <div className="image1"></div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Logo from "../Logo/Logo";
 import "./navbar.scss";
 
