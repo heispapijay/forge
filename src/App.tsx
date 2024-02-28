5ba5b84dd6c34ec458f56725b4964926683e242f
heispapijay <heispapijay@gmail.com>
2024-02-29 00:25:50 +0200

    Add react-spinners library and implement loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,31 @@ import "./App.css";
 import "./utils/resetstyle.scss";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./screens/Home/Home";
+import { useEffect, useState } from "react";
+import { ScaleLoader } from "react-spinners";
+import Logo from "./components/Logo/Logo";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
 
   return (
     <>
-      <Navbar />
-      <Home />
+    {loading ?
+    
+    <div className="loader"><ScaleLoader color={"#23262f"} loading={loading} />
+    <div className="loading"><Logo /></div></div>
+    :
+
+    <div>
+        <Navbar />
+        <Home />
+    </div>}
     </>
   );
 }
