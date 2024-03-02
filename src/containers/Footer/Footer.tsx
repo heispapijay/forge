867cd866468e91c832171566f8d4d9215afed55e
heispapijay <heispapijay@gmail.com>
2024-03-02 18:21:27 +0200

    Update Banner and Footer styles

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,7 +1,52 @@
-
+import { Link } from "react-router-dom";
+import Logo from "../../components/Logo/Logo";
+import facebook from "../../assets/facebook.svg";
+import dribble from "../../assets/dribbble.svg";
+import instagram from "../../assets/instagram.svg";
+import linkedin from "../../assets/linkedin.svg";
+import "./footer.scss";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="default-wrapper">
+        <div className="footer-container">
+          <div className="left-footer">
+            <div className="nav-logo">
+              <Link to="/">
+                <Logo />
+              </Link>
+            </div>
+            <p>
+              Forge is an award UI/UX designs and branding agency based in
+              Ocala, FL.
+            </p>
+            <div className="footer-links">
+              <a href="/">Home</a>
+              <a href="/about">About</a>
+              <a href="/work">Work</a>
+              <a href="#">Terms</a>
+              <a href="#">Policy</a>
+            </div>
+          </div>
+          <div className="right-footer">
+            <div className="social-links">
+              <a href="#"><img src={facebook} alt="facebook" /></a>
+              <a href="#"><img src={dribble} alt="dribble" /></a>
+              <a href="#"><img src={instagram} alt="instagram" /></a>
+              <a href="#"><img src={linkedin} alt="linkedin" /></a>
+            </div>
+            <div className="contact-info">
+              <p>hello@forgestudio.com</p>
+              <p>+1(352) 8844 0270 </p>
+            </div>
+          </div>
+        </div>
+        <div className="footer-buttom">
+          <p>&copy; 2024 Forge Studio. All rights reserved.</p>
+          <p className="developedby">Developed by <a href="https://opaleke.vercel.app/">Josiah Opaleke</a></p>
+        </div>
+      </div>
+    </div>
+  );
+};
